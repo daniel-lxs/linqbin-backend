2f13b560868ab70abdbc2dd3e9fdcb9a32630880
Daniel Riccio <ricciodaniel98@gmail.com>
2024-01-15 18:56:24 -0500

    remove comment

--- src/services/passkeyService.ts
+++ src/services/passkeyService.ts
@@ -1,6 +1,5 @@
 import { Entropy, charset64 } from 'entropy-string';
 
-//TODO: Encrypt entries
 export function generatePasskey() {
   const passkey = new Entropy({ charset: charset64, bits: 32 }).string();
   return passkey;
